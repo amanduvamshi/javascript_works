d177ea7bdfd03bae43df0453bda1348d20d131c3
vkama <krishna.amandu@gmail.com>
2025-05-10 18:48:44 +0530

    arrow function

---
+++ greet.js
@@ -0,0 +1,12 @@
+// ES5 function
+// function greet(name) {
+//     return "Hello, " + name + "!";
+// }
+// console.log(greet("Krishna")); // Output: Hello, Alice!
+
+
+//ES6 Arrow function
+const greet = (name) => {
+    return `Hello, ${name}!`;
+}
+console.log(greet("Krishna")); // Output: Hello, Alice!
