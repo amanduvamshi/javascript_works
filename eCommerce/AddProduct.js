90ca680868c74556d6a4ea76358bab46897c6ca3
vkama <krishna.amandu@gmail.com>
2025-05-11 10:21:00 +0530

    spread work..

--- eCommerce/AddProduct.js
+++ eCommerce/AddProduct.js
@@ -1,7 +1,8 @@
 const product1 = { id: 1, name: "Laptop", price: 999.99 };
+
 const product2 = { id: 2, name: "Smartphone", price: 499.99 };
 
-const cart = [product1, product2];
+let cart = [product1, product2];
 
 const product3 = { id: 3, name: "Tablet", price: 299.99 };
 
@@ -18,6 +19,20 @@ console.log(updatedCart);
 const { id, name, price } = product1;
 console.log(`Product ID: ${id}, Name: ${name}, Price: $${price}`);
 
-// Simplify bulk operations using spread operator
+// Destructuring allows you to extract values from objects or arrays and assign them to variables in a more concise way.
+// This is particularly useful when dealing with complex data structures, such as nested objects or arrays. 
+
+const addToCart = (...products) => {
+    cart = [...updatedCart, ...products]
+}
+
+const product5 = { id: 5, name: "Headphones", price: 199.99 };
+const product6 = { id: 6, name: "Charger", price: 29.99 };
+
+addToCart(product5, product6);
+console.log(cart);
+
+
+
 
 //Note: Using the spread operator (...cart) to add items to an array creates a new array each time. While this is fine for small arrays, it could impact performance for larger arrays. Consider using Array.prototype.push if mutability is acceptable.
